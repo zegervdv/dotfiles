848381be593bccf06ff3566398a6cbcd87811417
zegervdv <zegervdv@me.com>
2014-11-05 22:33:26 +0100

    Add oh-my-zsh dir moving aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -32,3 +32,23 @@ alias beer='bundle exec rake'
 
 # Ranger
 alias ranger="EDITOR=vim ranger"
+
+# Directories
+alias ..='cd ..'
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+alias cd.....='cd ../../../..'
+alias cd/='cd /'
+
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias d='dirs -v | head -10'

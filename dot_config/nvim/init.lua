b84adf68cd05bf42bd522c9caf34c0978ad927cd
Zeger Van de Vannet <zegervdv@me.com>
2020-12-17 15:00:49 +0100

    Update LSP bindings to work with telescope

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -86,13 +86,15 @@ local on_attach = function(client)
     vim.fn.nvim_buf_set_keymap(0, mode, key, result, {noremap=noremap, silent=true})
   end
 
-  vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.diagnostic.show_line_diagnostics()')
+  -- vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.diagnostic.show_line_diagnostics()')
+  mapper('n', '<CR>', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
   mapper('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>')
   mapper('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>')
   mapper('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
   mapper('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>')
   mapper('n', '1gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-  mapper('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  -- mapper('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  mapper('n', 'gr', "<cmd>lua require'telescope.builtin'.lsp_references()<CR>")
   mapper('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
   mapper('i', '<c-l>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
   mapper('n', '<leader>f', '<cmd>lua vim.lsp.buf.code_action()<CR>')
@@ -102,6 +104,7 @@ local on_attach = function(client)
   mapper("i", "<c-k>", "<Plug>(completion_prev_source)", false)
 end
 
+
 vim.lsp.handlers["textDocument/formatting"] = function(err, _, result, _, bufnr)
     if err ~= nil or result == nil then
         return
@@ -110,7 +113,7 @@ vim.lsp.handlers["textDocument/formatting"] = function(err, _, result, _, bufnr)
         local view = vim.fn.winsaveview()
         vim.lsp.util.apply_text_edits(result, bufnr)
         -- Fix to reload Treesitter
-        vim.api.nvim_command("noautocmd edit")
+        -- vim.api.nvim_command("edit")
         vim.fn.winrestview(view)
     end
 end

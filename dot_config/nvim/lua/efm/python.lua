e252f1b538112d15e9caeafd75d4cd7eb6b7e564
zegervdv <zegervdv@me.com>
2021-06-14 18:38:01 +0200

    Disable slow flake8 for now

--- dot_config/nvim/lua/efm/python.lua
+++ dot_config/nvim/lua/efm/python.lua
@@ -6,12 +6,12 @@ if (vim.fn.executable('black') == 1) then
   table.insert(languages.python, { formatCommand = 'black -', formatStdin = true })
 end
 
-if (vim.fn.executable('flake8') == 1) then
-  table.insert(languages.python, {
-    lintCommand = 'flake8 --stdin-display-name ${INPUT} -',
-    lintStdin = true,
-    lintIgnoreExitCode = true,
-    lintFormats = { '%f:%l:%c: %m' },
-  })
-end
+-- if (vim.fn.executable('flake8') == 1) then
+--   table.insert(languages.python, {
+--     lintCommand = 'flake8 --stdin-display-name ${INPUT} -',
+--     lintStdin = true,
+--     lintIgnoreExitCode = true,
+--     lintFormats = { '%f:%l:%c: %m' },
+--   })
+-- end
 

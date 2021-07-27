2baf0ec4c879b5a756af0f0db02a2eac7de62c75
zegervdv <zegervdv@me.com>
2021-07-27 18:31:48 +0200

    Use special char for indentline with no gaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -92,6 +92,7 @@ vim.defer_fn(function()
       config = function()
         vim.g.indent_blankline_buftype_exclude = { 'terminal', 'help', 'nofile' }
         vim.g.indent_blankline_show_first_indent_level = false
+        vim.g.indent_blankline_char = '│'
       end,
     }
 

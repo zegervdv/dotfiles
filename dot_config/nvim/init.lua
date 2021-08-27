83e05259fbf6b7b21cbbd354f5a419156119fab8
zegervdv <zegervdv@me.com>
2021-08-27 17:58:01 +0200

    Fix mapping for forced completion

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -211,7 +211,7 @@ vim.defer_fn(function()
             ['<C-n>'] = cmp.mapping.select_next_item(),
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-y>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm {
               behavior = cmp.ConfirmBehavior.Replace,

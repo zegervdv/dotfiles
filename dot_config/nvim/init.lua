18648d3903c4afb4c95a0dd2d1a9a402bffc3f01
Zeger Van de Vannet <zegervdv@me.com>
2021-09-01 11:13:53 +0200

    Disable autocomplete autoselection on CR

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -220,7 +220,6 @@ vim.defer_fn(function()
             ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm {
               behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
             },
           },
           sources = {

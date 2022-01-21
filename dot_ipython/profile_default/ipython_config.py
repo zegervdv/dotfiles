a9ea4cc2be112eada834b367c4aae59f93b24b69
zegervdv <zegervdv@me.com>
2022-01-21 15:51:10 +0100

    Set vi mode in ipython

---
+++ dot_ipython/profile_default/ipython_config.py
@@ -0,0 +1,4 @@
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShellApp.extensions = ["autoreload"]
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']

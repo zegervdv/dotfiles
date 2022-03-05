df93dc5c4104ff1316cdf034c2470e4c3e501ff1
zegervdv <zegervdv@me.com>
2022-03-05 11:05:54 +0100

    Set colorscheme in ipython

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -1,4 +1,6 @@
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.true_color = True
+c.TerminalInteractiveShell.highlighting_style = 'one-dark'
 c.InteractiveShellApp.extensions = ["autoreload"]
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']

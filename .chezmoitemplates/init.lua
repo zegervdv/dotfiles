e093998ef4999588e9ee36a4c3706a5b76b4785e
zegervdv <zegervdv@me.com>
2022-05-15 11:34:13 +0200

    Fix mappings for cmp

--- .chezmoitemplates/init.lua
+++ .chezmoitemplates/init.lua
@@ -302,41 +302,13 @@ require('packer').startup(function()
           end,
         },
         mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-y>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-          },
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, {
-            'i',
-            's',
-          }),
-
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, {
-            'i',
-            's',
-          }),
+          ['<C-p>'] = { i = cmp.mapping.select_prev_item() },
+          ['<C-n>'] = { i = cmp.mapping.select_next_item() },
+          ['<C-d>'] = { i = cmp.mapping.scroll_docs(-4) },
+          ['<C-y>'] = { i = cmp.mapping.complete() },
+          ['<C-e>'] = { i = cmp.mapping.close() },
+          ['<CR>'] = { i = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace } },
+          ['<C-k>'] = { i = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace } },
         },
         sources = {
           { name = 'nvim_lsp' },
@@ -352,7 +324,11 @@ require('packer').startup(function()
       }
 
       cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmp.mapping.preset.cmdline {
+          ['<C-p>'] = { c = cmp.mapping.select_prev_item() },
+          ['<C-n>'] = { c = cmp.mapping.select_next_item() },
+          ['<C-y>'] = { c = cmp.mapping.complete() },
+        },
         sources = cmp.config.sources({
           { name = 'path' },
         }, {

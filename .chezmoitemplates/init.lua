44f673a61f865d651abfe19fe8b31c521dcf2cc6
Zeger Van de Vannet <zegervdv@me.com>
2022-07-19 15:26:00 +0200

    wrap lines at 100 chars with black

--- .chezmoitemplates/init.lua
+++ .chezmoitemplates/init.lua
@@ -931,7 +931,7 @@ lsp.clangd.setup {
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.black.with { extra_args = { '--line-length', '100' } },
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.formatting.prettier,

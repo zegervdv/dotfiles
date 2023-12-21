f47a43696e02307b46671220cc75940f7f847996
zegervdv <zegervdv@me.com>
2023-12-21 11:47:51 +0100

    feat: enable highlighting for chezmoi .tmpl files

---
+++ dot_config/helix/runtime/queries/gotmpl/injections.scm
@@ -0,0 +1,7 @@
+((text) @injection.content
+  (#set! injection.language "bash")
+  (#set! injection.combined))
+
+((text) @injection.content
+  (#set! injection.language "toml")
+  (#set! injection.combined))
